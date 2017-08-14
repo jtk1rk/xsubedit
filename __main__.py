7b41db964045f2cce52314f0cff3917ba0ce236d
jtkirk <jtkirk@freemail.gr>
2017-08-14 23:02:58 +0300

    added __main__.py

---
+++ __main__.py
@@ -0,0 +1,18 @@
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('GObject', '2.0')
+from gi.repository import Gtk, GObject
+from model import Model
+from view import View
+from controller import Controller
+import sys
+
+#from utils import set_process_name
+#set_process_name()
+try:
+    GObject.threads_init()
+    win = Controller(Model(), View('xSubEdit 1.6.6'))
+    Gtk.main()
+except:
+    win.crash_save()
+    sys.exit()
