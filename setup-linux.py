64b0ecb0f7dd68331403dcad8180687ffaea9f27
James T. Kirk <jtkirk@freemail.gr>
2017-08-17 22:42:32 +0300

    linux-setup script update

--- setup-linux.py
+++ setup-linux.py
@@ -1,15 +1,18 @@
 from cx_Freeze import setup, Executable
 
+addtional_mods = ['numpy.core._methods', 'numpy.lib.format']
+
 setup(name = 'xSubEdit',
       description = 'xSubEdit subtitle editor',
       version = '1.5.3rc8',
       options = {"build_exe": {"build_exe": "dist/xSubEdit",
-                               "create_shared_zip": False,
-                               "packages": ["gi"],
-                               "excludes": ["Tkinter"]
+                               #"create_shared_zip": False,
+                               "packages": ["gi",'idna'],
+                               "excludes": ["Tkinter"],
+                               'includes': addtional_mods
                                }},
-      executables = [Executable(script = "main.py",
+      executables = [Executable(script = "__main__.py",
                                 targetName = "xSubEdit",
-                                appendScriptToExe = True,
+                                #appendScriptToExe = True,
                                 )]
     )
