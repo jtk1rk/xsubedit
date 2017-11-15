d6b3a663eba7b67d03b1b6926a5ba09b8ce7d9bb
James T. Kirk <jtk1rk@freemail.gr>
2017-11-16 00:43:35 +0200

    win updater 4

--- win_updater.py
+++ win_updater.py
@@ -6,6 +6,7 @@ import os
 from os.path import split
 from filelist_generator import gen_filelist
 from cfile import cfile
+import platform
 
 BASE_URL = 'https://raw.githubusercontent.com/jtk1rk/xsubedit/master'
 
@@ -28,6 +29,8 @@ def main():
     for line in remote_list:
         filename, md5 = line.split('|')
         filename = filename.strip()
+        if platform.system() == 'Windows':
+            filename = filename.replace('/','\\')
         md5 = md5.strip()
         remote_dict[filename] = md5
     for line in local_list:
@@ -39,9 +42,11 @@ def main():
     dl = []
     for key in remote_dict:
         if not (key in local_dict) or local_dict[key] != remote_dict[key]:
-            dl.append((BASE_URL+'/'+key[1:], key))
+            #print ((BASE_URL+'/'+ key[1:] if platform.system() != 'Windows' else key[1:].replace('\\','/'), key))
+            dl.append((BASE_URL+'/'+key[1:] if platform.system() != 'Windows' else key[1:].replace('\\','/'), key))
 
     for idx, item in enumerate(dl):
+        print(item[1])
         print ('Downloading: %s (%.2f)' % (split(item[1])[1], (idx+1) / len(dl) ) ,)
         f = cfile(item[1])
         if not f.path_exists:
