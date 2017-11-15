0652c6a3e9fb6f36e2307b51db9bb05c1063c9c0
James T. Kirk <jtk1rk@freemail.gr>
2017-11-15 21:53:43 +0200

    win updater

--- win_updater.py
+++ win_updater.py
@@ -1,35 +1,39 @@
 # Windows xSubEdit Project Updater
 
 import urllib.request
+import requests
 import os
 from os.path import split
 from filelist_generator import gen_filelist
 from cfile import cfile
 
-BASE_URL = ''
+BASE_URL = 'https://raw.githubusercontent.com/jtk1rk/xsubedit/master'
 
 def get_file(url, filename):
     urllib.request.urlretrieve(url, filename)
 
 def read_remote_file(url):
-    return urllib.urlopen(url).readlines()
+    return requests.get(url).text.split()
 
 def read_filelist(filename):
     with open(filename, 'r') as f:
         return f.readlines()
 
 def main():
-    local_list = gen_filelist(write_file = false)
+    local_list = gen_filelist(write_file = False)
+    print(BASE_URL+'/filelist.txt')
     remote_list = read_remote_file(BASE_URL+'/filelist.txt')
 
     local_dict = {}
     remote_dict = {}
     for line in remote_list:
+        print(line)
         filename, md5 = line.split('|')
         filename = filename.strip()
         md5 = md5.strip()
         remote_dict[filename] = md5
     for line in local_list:
+        print(line)
         filename, md5 = line.split('|')
         filename = filename.strip()
         md5 = md5.strip()
@@ -41,7 +45,7 @@ def main():
             dl.append((BASE_URL+'/'+key[1:], key))
 
     for idx, item in enumerate(dl):
-        print ('Downloading: %s (%.2f)' % (split(item[1])[1], idx / len(dl) ) ,)
+        print ('Downloading: %s (%.2f)' % (split(item[1])[1], idx+1 / len(dl) ) ,)
         #f = cfile(item[1])
         #if not f.path_exists:
         #    f.create_path()
